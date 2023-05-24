80e48141148e31c11016540d8a98321c31709315
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-05-24 07:59:36 +0300

    Add display score table

---
+++ src/modules/display.js
@@ -0,0 +1,21 @@
+const display =
+    `
+    
+    <div>
+        <h2>Recent scores</h2>
+        <button type="button">Refresh</button>
+    </div>
+    <ul>
+        <li>Name: 100</li>
+        <li>Name: 20</li>
+        <li>Name: 50</li>
+        <li>Name: 78</li>
+        <li>Name: 125</li>
+        <li>Name: 100</li>
+        <li>Name: 77</li>
+        <li>Name: 42</li>
+    </ul>
+
+    `
+
+export default display
